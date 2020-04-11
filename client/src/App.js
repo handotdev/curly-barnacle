977240b5b9606da63124e34f79a4c586b153627e
Han Wang <hyw2@cornell.edu>
2020-04-11 04:31:56 -0400

    Add ESlint and Prettier Configurations

--- client/src/App.js
+++ client/src/App.js
@@ -12,22 +12,35 @@ const { Header, Content, Footer } = Layout;
 function App() {
   return (
     <div>
-    <Layout className="layout">
-      <Header style={{color: '#FFF'}}>
-        Zoom University@Cornell
-      </Header>
-      <Content>
-        <div className="site-layout-content" style={{padding: '25px 50px 85px 50px'}}>
-        
-        <h1><BellTwoTone /> Receive email reminders with Zoom invites to your classes</h1>
-        {/* Add Product Info */}
-        <Window />
-        <Divider>Sign Up</Divider>
-        <Fields />
-        </div>
-      </Content>
-    </Layout>
-    <Footer style={{ textAlign: 'center', width: '100%', position: 'fixed', bottom: '0' }}>Built with ❤️ by Ansh Godha & Han Wang wishing everyone well in these uncertain times</Footer>
+      <Layout className="layout">
+        <Header style={{ color: '#FFF' }}>Zoom University@Cornell</Header>
+        <Content>
+          <div
+            className="site-layout-content"
+            style={{ padding: '25px 50px 85px 50px' }}
+          >
+            <h1>
+              <BellTwoTone /> Receive email reminders with Zoom invites to your
+              classes
+            </h1>
+            {/* Add Product Info */}
+            <Window />
+            <Divider>Sign Up</Divider>
+            <Fields />
+          </div>
+        </Content>
+      </Layout>
+      <Footer
+        style={{
+          textAlign: 'center',
+          width: '100%',
+          position: 'fixed',
+          bottom: '0',
+        }}
+      >
+        Built with ❤️ by Ansh Godha & Han Wang wishing everyone well in these
+        uncertain times
+      </Footer>
     </div>
   );
 }
