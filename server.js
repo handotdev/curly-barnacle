7e33f82607230e7023f513785d6067f633ce2c16
Han Wang <hyw2@cornell.edu>
2020-04-13 00:49:45 -0400

    Add regrouping and edge case considerations

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
-// const scraper = require('./scraper.js')
-
 const functions = require('./functions/index.js')
 
 app.use(cors());
 
-app.get('/api/firebase-test', (req, res) => {
-    functions.handleFormSubmission('hyw2@cornell.edu', 'https://classes.cornell.edu/shared/schedule/SP20/92eac951cf5b329be2522a9829421833')
+app.get('/api/schedule', (req, res) => {
+    const { email, id } = req.query;
+    functions.handleFormSubmission(email, id)
         .then(_ => res.send('success'))
         .catch(err => console.log(err))
 })
 
-const PORT = 3333;
+const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
