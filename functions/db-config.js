0a806719665aba9ecc8760ddc15e03b3af9f638f
Han Wang <hyw2@cornell.edu>
2020-04-16 05:25:36 -0400

    add env config changes

--- functions/db-config.js
+++ functions/db-config.js
@@ -2,20 +2,7 @@ const admin = require('firebase-admin');
 require('dotenv').config();
 //import * as async from 'async'
 
-const { FB_TYPE, FB_PROJECT_ID, FB_PRIVATE_KEY_ID, FB_PRIVATE_KEY, FB_CLIENT_EMAIL, FB_CLIENT_ID, FB_AUTH_URI, FB_TOKEN_URI, FB_AUTH_PROVIDER, FB_CLIENT_CERT_URL } = process.env;
-
-serviceAccount = {
-  "type": FB_TYPE,
-  "project_id": FB_PROJECT_ID,
-  "private_key_id": FB_PRIVATE_KEY_ID,
-  "private_key": FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  "client_email": FB_CLIENT_EMAIL,
-  "client_id": FB_CLIENT_ID,
-  "auth_uri": FB_AUTH_URI,
-  "token_uri": FB_TOKEN_URI,
-  "auth_provider_x509_cert_url": FB_AUTH_PROVIDER,
-  "client_x509_cert_url": FB_CLIENT_CERT_URL
-}
+serviceAccount = JSON.parse(process.env.FIRE_CREDENTIALS);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
