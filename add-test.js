a0edeea773907cff5c69109c447f7c22a929004d
Han Wang <hyw2@cornell.edu>
2020-04-16 03:50:40 -0400

    Add env configuration

--- add-test.js
+++ add-test.js
@@ -25,10 +25,10 @@ const toAdd = {
 const times = ['3:40', '3:45', '3:50', '3:55', '4:00', '4:05'];
 
 times.forEach((time) => {
-    functions.insertData(collectionsRef, "W "+time+"AM", 'ag759@cornell.edu', toAdd.data).then((res) => {
+    functions.insertData(collectionsRef, "T "+time+"AM", 'ag759@cornell.edu', toAdd.data).then((res) => {
         console.log("Successfully add doc for ag759@cornell.edu at "+time);
     });
-    functions.insertData(collectionsRef, "W "+time+"AM", 'hyw2@cornell.edu', toAdd.data).then((res) => {
+    functions.insertData(collectionsRef, "T "+time+"AM", 'hyw2@cornell.edu', toAdd.data).then((res) => {
         console.log("Successfully add doc for hyw2@cornell.edu at "+time);
     });
 })
